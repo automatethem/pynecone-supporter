f7c0a448d70234e699693ad8f2b45fac8cbc43da
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-04 22:39:08 +0900

    Update color_picker.py

--- pynecone_helper/components/color_picker.py
+++ pynecone_helper/components/color_picker.py
@@ -1,8 +1,8 @@
 import pynecone as pc
 
 '''
-#https://github.com/omgovich/react-colorful#getting-started
 #https://www.npmjs.com/package/react-colorful
+#reactjs 컴포넌트 사용 자바스크립트 코드
 import { HexColorPicker } from "react-colorful";
 
 const YourComponent = () => {
@@ -10,12 +10,12 @@ const YourComponent = () => {
   return <HexColorPicker color={color} onChange={setColor} />;
 };
 '''
-class ColorPicker(pc.Component): #pc.Component 상속
-    library = "react-colorful" #npm 패키지 이름
-    tag = "HexColorPicker" #리액트 컴포넌트의 태그 이름
+class ColorPicker(pc.Component):
+    library = "react-colorful" #from 뒤 npm 패키지 이름
+    tag = "HexColorPicker" #import 뒤 리액트 컴포넌트의 태그 이름
 
     @classmethod  
     def get_controlled_triggers(cls):
-        return {"on_change"}
+        return {"on_change"} #onChange -> on_change
 
 color_picker = ColorPicker.create
