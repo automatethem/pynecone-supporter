a51fd35d18e1291ee1b335029f3918a1a7732324
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-05 14:35:12 +0900

    Update color_picker.py

--- pynecone_helper/components/color_picker.py
+++ pynecone_helper/components/color_picker.py
@@ -1,15 +1,6 @@
 import pynecone as pc
 
-'''
 #https://www.npmjs.com/package/react-colorful
-#react-colorful reactjs 컴포넌트 사용 자바스크립트 코드
-import { HexColorPicker } from "react-colorful";
-
-const YourComponent = () => {
-  const [color, setColor] = useState("#aabbcc");
-  return <HexColorPicker color={color} onChange={setColor} />;
-};
-'''
 class ColorPicker(pc.Component):
     library = "react-colorful" #from 뒤 npm 패키지 이름
     tag = "HexColorPicker" #import 뒤 리액트 컴포넌트의 태그 이름
