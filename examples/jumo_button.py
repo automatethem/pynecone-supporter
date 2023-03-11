78a2f370811ed06abde4a5b5f8db611cb2119e1f
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-11 10:43:55 +0900

    Update jumo_button.py

--- examples/jumo_button.py
+++ examples/jumo_button.py
@@ -1,1 +1,21 @@
+import pynecone as pc
+import pynecone_helper
 
+class State(pc.State):
+    #color = "#aabbcc"
+    pass
+
+def index():
+    return pc.box(
+        pc.vstack(
+            pc.heading(State.color),
+            pynecone_helper.jumo_button(on_change=State.set_color),
+        ),
+        #background_color=State.color,
+        padding="5em",
+        border_radius="1em",
+    )
+
+app = pc.App(state=State)
+app.add_page(index, title="Jumo button")
+app.compile()
