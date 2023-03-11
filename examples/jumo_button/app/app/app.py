9717cfeed2cbd64a7ca4201e2250126900461cf8
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-11 11:56:32 +0900

    Update app.py

--- examples/jumo_button/app/app/app.py
+++ examples/jumo_button/app/app/app.py
@@ -1,15 +1,19 @@
 import pynecone as pc
 import pynecone_helper
 
+class State(pc.State):
+    background_color = "#aabbcc"
+    font_color = "#aabbcc"
+    
 def index():
     #print(pynecone_helper.jumo_button(background_color='red', font_color= "#aabbcc")) #<JumoButton fontColor="#aabbcc" backgroundColor="red"/>
-    #print(type(jumo_button(background_color='red', font_color= "#aabbcc"))) #<class 'test.test.JumoButton'>    
+    #print(type(pynecone_helper.jumo_button(background_color='red', font_color= "#aabbcc"))) #<class 'test.test.JumoButton'>    
     return pc.box(
         pc.vstack(
             pc.text(State.color),
             pynecone_helper.jumo_button(background_color='red', font_color= "#aabbcc"),
         ),
-        #background_color=State.color,
+        background_color=State.background_color,
         padding="5em",
         border_radius="1em",
     )
