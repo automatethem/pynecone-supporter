72d4f88afdef9cde2a4499ab551f5eb45ede166d
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-16 20:21:28 +0900

    Update __init__.py

--- pynecone_supporter/components/__init__.py
+++ pynecone_supporter/components/__init__.py
@@ -1,3 +1,7 @@
 from . import color_picker
+from . import jumo_button
+from . import json_editor
 
 from .color_picker import color_picker
+from .color_picker import jumo_button
+from .color_picker import json_editor
