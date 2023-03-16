63e4376f475fb32daabba94dca4d632cd2ecb3b9
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-16 20:37:09 +0900

    Update app.py

--- examples/json_editor/app/app/app.py
+++ examples/json_editor/app/app/app.py
@@ -11,7 +11,7 @@ def index():
     return pc.box(
         pc.vstack(
             pc.text(State.json_object),
-            pynecone_supporter.components.json_editor(jsonObject=State.jsonObject, on_change=State.set_data),
+            pynecone_supporter.components.json_editor(jsonObject=State.json_object, on_change=State.set_json_object),
         ),
         padding="5em",
         border_radius="1em",
