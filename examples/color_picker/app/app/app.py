39ae7e229006c21ae573ec5bb996504d9fdc4f29
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-16 15:49:02 +0900

    Update app.py

--- examples/color_picker/app/app/app.py
+++ examples/color_picker/app/app/app.py
@@ -1,16 +1,16 @@
 import pynecone as pc
-import pynecone_helper
+import pynecone_supporter
 
 class State(pc.State):
     color = "#aabbcc"
 
 def index():
-    #print(pynecone_helper.color_picker(on_change=State.set_color)) #<HexColorPicker onChange={(_e) => Event([E("state.set_color", {value:_e})])}/>
-    #print(type(pynecone_helper.color_picker(on_change=State.set_color))) #<class 'test.test.ColorPicker'>  
+    #print(pynecone_supporter.color_picker(on_change=State.set_color)) #<HexColorPicker onChange={(_e) => Event([E("state.set_color", {value:_e})])}/>
+    #print(type(pynecone_supporter.color_picker(on_change=State.set_color))) #<class 'test.test.ColorPicker'>  
     return pc.box(
         pc.vstack(
             pc.text(State.color),
-            pynecone_helper.color_picker(on_change=State.set_color),
+            pynecone_supporter.color_picker(on_change=State.set_color),
         ),
         background_color=State.color,
         padding="5em",
