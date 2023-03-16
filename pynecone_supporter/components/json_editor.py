c8558d4dfb1cf7fd7c1ac05686102c5ad8c8b949
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-16 15:38:57 +0900

    Update json_editor.py

--- pynecone_supporter/components/json_editor.py
+++ pynecone_supporter/components/json_editor.py
@@ -1,1 +1,15 @@
+import pynecone as pc
+
 #https://www.npmjs.com/package/rc-json-editor
+class JsonEditor(pc.Component):
+    library = "rc-json-editor" #from 뒤 npm 패키지 이름
+    tag = "JsonEditor" #import 뒤 리액트 컴포넌트의 태그 이름
+    #리액트 속성에 대응
+    data: pc.Var[str] #data
+
+    #리액트 이벤트에 대응
+    @classmethod
+    def get_controlled_triggers(cls) -> dict[str, pc.Var]:
+        return {"on_change": pc.EVENT_ARG} #onChange
+
+json_editor = JsonEditor.create
