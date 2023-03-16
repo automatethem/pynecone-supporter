207ddc0f2797161f85f2cb614f30e868bb0db6c2
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-16 20:28:38 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def requirements():
 
 setuptools.setup(
 	name='pynecone-supporter',
-	version='0.0.3',
+	version='0.0.4',
 	description='Pynecone supporter',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
