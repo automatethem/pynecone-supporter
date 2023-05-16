1c561e53022f6898fb5a1f4e6af8689b1fa06376
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-05-16 21:49:18 +0900

    Update json_editor.py

--- pynecone_supporter/components/json_editor.py
+++ pynecone_supporter/components/json_editor.py
@@ -4,6 +4,7 @@ import pynecone as pc
 class JsonEditor(pc.Component):
     library = "react-jsondata-editor" #from 뒤 npm 패키지 이름
     tag = "JsonEditor" #import 뒤 리액트 컴포넌트의 태그 이름
+    
     #리액트 속성에 대응
     json_object: pc.Var[str] 
 
