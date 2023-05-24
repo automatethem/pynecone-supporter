8956ba52fd40926416d1b32c992a29663e659fc7
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-05-24 17:19:29 +0900

    Update __init__.py

--- pynecone_supporter/components/__init__.py
+++ pynecone_supporter/components/__init__.py
@@ -1,9 +1,9 @@
-from . import color_picker
-from . import jumo_button
-from . import json_editor
-from . import webcam
+#from . import color_picker
+#from . import jumo_button
+#from . import json_editor
+#from . import webcam
 
-#from .color_picker import ColorPicker
-#from .jumo_button import JumoButton
-#from .json_editor import JsonEditor
-#from .webcam import Webcam
+from .color_picker import color_picker
+from .jumo_button import jumo_button
+from .json_editor import json_editor
+from .webcam import webcam
