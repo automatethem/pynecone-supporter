59ceabc86b7705ecf0471a14e61978b77bb7bbff
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-19 14:47:04 +0900

    Update app.py

--- examples/json_editor/app/app/app.py
+++ examples/json_editor/app/app/app.py
@@ -1,20 +1,20 @@
-import pynecone as pc
-import pynecone_supporter
+import reflex as pc
+import reflex_supporter
 
-class State(pc.State):
+class State(rx.State):
     #json_object = """{ "aaa": "bbb", "ddfd": "fgfg" }"""
     json_object = """[{ "aaa": "bbb", "ddfd": "fgfg" }]"""
 
 def contents():
-    return pc.box(
-        pc.vstack(
-            pc.text(State.json_object),
+    return rx.box(
+        rx.vstack(
+            rx.text(State.json_object),
             pynecone_supporter.components.json_editor(json_object=State.json_object, on_change=lambda json_object: State.set_json_object(json_object)),
         ),
         padding="5em",
         border_radius="1em",
     )
 
-app = pc.App(state=State)
+app = rx.App(state=State)
 app.add_page(contents, route='/', title="Json editor")
 app.compile()
