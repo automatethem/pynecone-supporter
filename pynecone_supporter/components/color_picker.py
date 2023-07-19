37ed2adbb66a7d200b3f741bfcb919da7a5c1722
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-19 14:48:22 +0900

    Update color_picker.py

--- pynecone_supporter/components/color_picker.py
+++ pynecone_supporter/components/color_picker.py
@@ -1,18 +1,18 @@
-import pynecone as pc
+import reflex as rx
 
 #https://www.npmjs.com/package/react-colorful
-class ColorPicker(pc.Component):
+class ColorPicker(rx.Component):
     library = "react-colorful" #from 뒤 npm 패키지 이름
     tag = "HexColorPicker" #import 뒤 리액트 컴포넌트의 태그 이름
     
     #리액트 속성
-    color: pc.Var[str]
+    color: rx.Var[str]
 
     #리액트 이벤트
     @classmethod
     def get_controlled_triggers(cls):
         return {
-            "on_change": pc.EVENT_ARG
+            "on_change": rx.EVENT_ARG
         }
 
 color_picker = ColorPicker.create
