c794860ec0d6a519152d524fda610113388043c3
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-19 14:47:36 +0900

    Update app.py

--- examples/jumo_button/app/app/app.py
+++ examples/jumo_button/app/app/app.py
@@ -1,14 +1,14 @@
-import pynecone as pc
-import pynecone_supporter
+import reflex as rx
+import reflex_supporter
 
-class State(pc.State):
+class State(rx.State):
     background_color = "#aabbcc"
     font_color = "#aabbcc"
     
 def contents():
-    return pc.box(
-        pc.vstack(
-            pc.text(State.background_color),
+    return rx.box(
+        rx.vstack(
+            rx.text(State.background_color),
             pynecone_supporter.components.jumo_button(background_color='red', font_color= "#aabbcc"),
         ),
         background_color=State.background_color,
@@ -16,6 +16,6 @@ def contents():
         border_radius="1em",
     )
 
-app = pc.App(state=State)
+app = rx.App(state=State)
 app.add_page(contents, route="/", title="Jumo button")
 app.compile()
