c098225003e47e5a8c4acc9b8c3a2d3e45c18d0e
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-19 14:49:17 +0900

    Update jumo_button.py

--- pynecone_supporter/components/jumo_button.py
+++ pynecone_supporter/components/jumo_button.py
@@ -1,19 +1,19 @@
-import pynecone as pc
+import reflex as rx
 
 #https://www.npmjs.com/package/react-supporter
-class JumoButton(pc.Component):
+class JumoButton(rx.Component):
     library = "react-supporter" #from 뒤 npm 패키지 이름
     tag = "JumoButton" #import 뒤 리액트 컴포넌트의 태그 이름
     
     #리액트 속성
-    background_color: pc.Var[str]
-    font_color: pc.Var[str]
+    background_color: rx.Var[str]
+    font_color: rx.Var[str]
 
     #리액트 이벤트
     @classmethod
     def get_controlled_triggers(cls):
         return {
-            "on_click": pc.EVENT_ARG
+            "on_click": rx.EVENT_ARG
         } 
 
 jumo_button = JumoButton.create
