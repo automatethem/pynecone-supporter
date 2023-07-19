13673e87be43e59ba0ad86dd1ab5e805456ca958
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-19 14:49:37 +0900

    Update webcam.py

--- pynecone_supporter/components/webcam.py
+++ pynecone_supporter/components/webcam.py
@@ -1,7 +1,7 @@
-import pynecone as pc
+import reflex as rx
 
 #https://www.npmjs.com/package/react-webcam
-class Webcam(pc.Component):
+class Webcam(rx.Component):
     library = "react-webcam" #from 뒤 npm 패키지 이름
     tag = "Webcam" #import 뒤 리액트 컴포넌트의 태그 이름
 
